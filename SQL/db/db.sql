8db33e6c7734f542933c1ca858443de9a05cd342
Aaron Cox <aaroncox@aarons-imac.lan>
2022-09-05 16:41:52 -0600

    commit"

---
+++ SQL/db/db.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS clients;
+CREATE DATABASE clients;
+USE clients;
---
+++ SQL/db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO customers (first_name, last_name)
+VALUES
+  ('Ronald', 'Firbank'),
+  ('Virginia', 'Woolf'),
+  ('Piers', 'Gaveston'),
+  ('Charles', 'LeRoi'),
+  ('Katherine', 'Mansfield'),
+  ('Dora', 'Carrington'),
+  ('Edward', 'Bellamy'),
+  ('Montague', 'Summers'),
+  ('Octavia', 'Butler'),
+  ('Unica', 'Zurn'),
+  ('Mitch', 'Johnson'),
+    ('Kevin', 'Goff'),
+    ('Bevin', 'Johnson'),
+    ('Mustafa', 'Sameric'); 
