10bfdaaa680701b4a7b6e24f55289e9dfe9687c8
Aaron Cox <aaroncox@aarons-imac.lan>
2022-09-10 19:46:38 -0600

    getting front-end incorporated

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const homeRoutes = require('./home-routes.js');
 
 
 router.use(require('./customerRoutes'));
 router.use(require('./machineRoutes'));
-
+router.use('/', homeRoutes);
 module.exports = router;
--- server.js
+++ server.js
@@ -2,19 +2,30 @@ const express = require('express');
 const db = require('./SQL/db/connection')
 const apiRoutes = require('./routes/apiRoutes');
 
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 //express middleware 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use('/api', apiRoutes);
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./routes/apiRoutes'));
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
+
+
 
 // Start server after DB connection
 db.connect(err => {
